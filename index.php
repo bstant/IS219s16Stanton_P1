0006e239aad5318f5ddeaa7bdcc78b1209f45738
bstant <bms29@njit.edu>
2016-02-19 23:55:12 -0500

    Added files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Brian Stanton Portfolio</title>
+    <!--REQUIRED STYLE SHEETS-->
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLE CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- VEGAS STYLE CSS -->
+    <link href="assets/scripts/vegas/jquery.vegas.min.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css' />
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               
+            </div>
+            <!-- Collect the nav links for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#home">HOME</a>
+                    </li>
+                    <li><a href="#about">About Me</a>
+                    </li>
+
+                 
+                    <li><a href="#contact-sec">CONTACT</a>
+                    </li>
+                    <li><a href="#"><strong>e-mail:</strong>  bms29@njit.edu.com</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!--End Navigation -->
+
+
+    <!--Header section  -->
+    <div class="container" id="home">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1 class="head-main">Brian Stanton</h1>
+                <h2 class="head-sub-main">Portfolio Site</h2>
+                <a class="btn btn-danger btn-lg " href="#about">Read More </a><a class="btn btn-default btn-lg" href="#contact-sec">Leave Here </a>
+                <div class="head-last"></div>
+
+            </div>
+
+
+        </div>
+    </div>
+    <!--End Header section  -->
+
+
+    <!--words Section-->
+    <section class="for-full-back color-white" id="about">
+        <div class="container">
+
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>A little about me</h1>
+                    <h4>
+                        <strong>
+I am currently a Junior at NJIT studying Human Computer Interaction with a Specialization in Web and Multimedia design.
+I'm originally from West Paterson, NJ.
+                        </strong>
+                    </h4>
+                </div>
+
+            </div>
+
+            <div class="row text-center space-pad">
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+
+
+                        <h3> <a href="http://web.njit.edu/~bms29/CMBGroup">Coffee Meets Bagel Prototype</a></br></h3>
+                        <p>
+                            Prototype of the app "Coffee Meets Bagel"
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+                        
+
+
+                        <h3><a href="http://web.njit.edu/~bms29/117_site">Portfolio Site</a></br> </h3>
+                        <p>
+                            Another portfolio site I made in Axure
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 ">
+
+
+
+                </div>
+            </div>
+            <div class="row text-center">
+ 
+
+            </div>
+        </div>
+
+
+    </section>
+
+    <section class="for-full-back " id="Parallax-one">
+        <div class="container">
+
+            <div class="row text-center for-parallax-one">
+                <div class="col-md-6 ">
+                    <h3>Current</h3>
+                    <h4>
+                        <strong>Currently I work at a bank in Wayne as a teller and I have management experience at a retaurant.
+                        </strong>
+                    </h4>
+                </div>
+                <div class="col-md-6 ">
+                    <h3>Future</h3>
+                    <h4>
+                        <strong>This summer I have an internship with the Red Cross lined up.
+                        </strong>
+                    </h4>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <!--./parallax one-->
+    <!--End words Section-->
+
+
+
+
+
+    <!-- Contact Section -->
+    <section class="for-full-back color-white " id="contact-sec">
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-md-5 contact-cls">
+                    <h3>CONTACT ME </h3>
+                    <div>
+                        <span><i class="fa fa-home"></i>&nbsp; Address: Woodland Park</span>
+                        <br />
+                        <span><i class="fa fa-phone"></i>&nbsp;Phone: 973-508-3231</span>
+                        <br />
+                        <span><i class="fa fa-envelope-o"></i>&nbsp;E-Mail: bms29@njit.educom</span>
+                        <br />
+                    </div>
+                    <br />
+                    <div id="social-icon">
+                        <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-linkedin fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <h2>Leave me a message!</h2>
+                    <p>
+                       
+                       
+                    </p>
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required="required" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required="required" placeholder="Email address">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <textarea name="message" id="message" required="required" class="form-control" rows="3" placeholder="Message"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-default">Submit Request</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!--End Contact Section -->
+    <!--footer Section -->
+    <div class="for-full-back " id="footer">
+        2016 | All Right Reserved
+         
+    </div>
+    <!--End footer Section -->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/plugins/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP CORE SCRIPT   -->
+    <script src="assets/plugins/bootstrap.js"></script>
+    <!-- VEGAS SLIDESHOW SCRIPTS -->
+    <script src="assets/plugins/vegas/jquery.vegas.min.js"></script>
+    <script src="assets/plugins/jquery.parallax-1.1.3.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
